9adc56a9872e1bbce6c3730a13369c3b5334160c
Didenko Yura <didenkot34@gmail.com>
2016-06-19 15:28:30 +0300

    chenged view layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,70 +1,14 @@
 @include('layouts.header')
 <body id="app-layout">
-<nav class="navbar navbar-default navbar-static-top" id="top">
-    <div class="container">
-        <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand " href="{{ url('/') }}">
-                <i class="fa fa-video-camera premiere" aria-hidden="true"></i>
-                <span class="premiere text-uppercase">sho-kinopoisk</span>
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                {{--<li><a href="{{ url('/home') }}">Home</a></li>--}}
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            <li><a href="{{ url('/admin/users') }}"><i class="fa fa-btn fa-lock  "></i>Admin</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
-        </div>
-    </div>
-</nav>
+@include('layouts.navbar')
 
 @yield('content')
 
-<div class="scroll-top-wrapper ">
-  <span class="scroll-top-inner">
-    <i class="fa fa-2x fa-arrow-circle-up"></i>
-  </span>
-</div>
+{{--Scroll Top Button--}}
+@include('layouts.scrollTop')
+{{--END Scroll Top Button--}}
 <!-- Footer -->
-<footer class="footer">
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; Sho-kinopoisk {{date('Y-m-d')}}</p>
-        </div>
-    </div>
-    <!-- /.row -->
-</footer>
+@include('layouts.footer')
 <!-- JavaScripts -->
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
 <script src="/assets/js/jquery-2.2.3.min.js"></script>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="footer">
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Sho-kinopoisk {{date('Y-m-d')}}</p>
+        </div>
+    </div>
+    <!-- /.row -->
+</footer>
