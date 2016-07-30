b09f5bfadc7a586fe041b554347dc3610e1bc144
Didenko Yura <didenkot34@gmail.com>
2016-07-31 01:14:17 +0300

    Added previous url to redirect in facebook

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -9,18 +9,22 @@ use Socialite;
 
 class SocialAuthController extends Controller
 {
-    public function redirect()
+    public function redirect(Request $request)
     {
+        session(['previousUrl' => $request->session()->previousUrl()]);
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function callback(SocialAccountService $service)
+    public function callback(SocialAccountService $service, Request $request)
     {
 
         $user = $service->createOrGetUser(Socialite::driver('facebook')->user());
 
         auth()->login($user);
-
+        if ($request->session()->has('previousUrl')) {
+            
+            return redirect(session('previousUrl'));
+        }
         return redirect()->to('/');
     }
 }
