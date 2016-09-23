d55d4d7b6a3b4e55bff492fd2f5dc15023b161eb
Didenko Yura <didenkot34@gmail.com>
2016-09-23 21:00:04 +0300

    Added title template for box in empty serch

--- resources/views/search/empty.blade.php
+++ resources/views/search/empty.blade.php
@@ -5,10 +5,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-10 col-md-offset-1 box-shadow-jumbotron">
-                <h1 class="premiere"> {{Lang::get('title.editorsChoice')}}</h1>
-
-                <p class="lead">{{Lang::get('title.editorsChoiceDescription')}}</p>
-
+                @include('titleForBox' , [
+              'title' => Lang::get('title.editorsChoice'),
+              'titleDescription' => Lang::get('title.editorsChoiceDescription')
+              ])
                 <div class="slider4" data-slick='{"slidesToShow": 3, "slidesToScroll":3}'>
                     @each('trailer.trailersSlider', $editorsChoice, 'trailer')
                 </div>
