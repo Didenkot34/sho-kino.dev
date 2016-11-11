14286635f2fd0bfc1062144083940c242d4c6304
Didenko Yura <didenkot34@gmail.com>
2016-11-11 21:08:57 +0200

    Added script blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,7 @@
     @include('auth.popup.authorization')
 <!-- JavaScripts -->
     {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
-    <script src="/assets/js/jquery-2.2.3.min.js"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
-    <script src="/assets/js/slick/slick.min.js"></script>
-    <script src="/assets/js/app.min.js"></script>
-    <script src="/assets/js/bootstrap-dropdownhover.min.js"></script>
+    @include('layouts.script')
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </div>
 
---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,8 @@
+<!-- JavaScripts -->
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>--}}
+<script src="/assets/js/jquery-2.2.3.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/slick/slick.min.js"></script>
+<script src="/assets/js/app.min.js"></script>
+<script src="/assets/js/bootstrap-dropdownhover.min.js"></script>
+{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
